30bfd08afe6437177cd06afdbdc7e8db881d221a
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-12-01 07:10:53 +0300

    Completed assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user] ||= user
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def show
+    user = User.find_by(session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Sorry buddy! Not Authorized..." }, status: :unprocessable_entity
+
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
+end
